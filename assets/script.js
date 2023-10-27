03dd98a887d99e5e4f1d29c558d0393563250246
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2023-10-27 09:06:34 +0200

    Feat : suite

--- assets/script.js
+++ assets/script.js
@@ -13,24 +13,24 @@ function FormatPrice(price) {
 
 }
 
-function FormValidation() {
+document.querySelector("#form-expense").onsubmit = function FormValidation() {
 
+    event.preventDefault();
     let name = document.getElementById("input_name");
     let price = document.getElementById("input_price");
     let description = document.getElementById("input_desc");
     if (name.value === "") {
 
         alert("S'il vous plait entrer quelques choses");
+        document.querySelector("#messageAlert").innerHTML =
+            "<div class=\"alert alert-danger\" role=\"alert\">S'il vous plait entrer quelques choses</div>";
         return false;
 
     } else if (price.value === "") {
 
         alert("S'il vous plait entrer quelques choses");
-        return false;
-
-    } else if (description.value === "") {
-
-        alert("S'il vous plait entrer quelques choses");
+        document.querySelector("#messageAlert").innerHTML =
+            "<div class=\"alert alert-danger\" role=\"alert\">S'il vous plait entrer quelques choses</div>";
         return false;
 
     } else {
@@ -39,11 +39,21 @@ function FormValidation() {
         let resPrice = FormatPrice(price.value);
         if (resName && resPrice) {
 
+            const documentTr = document.createElement('tr');
+            documentTr.innerHTML =
+                `<td>${name.value}</td>
+                <td>${description.value}</td>
+                <td data-price>${price.value}</td>
+                <td><a class='text-danger' href='#' data-delete>X</a></td>`;
+            document.querySelector('tbody').appendChild(documentTr);
+            document.querySelector('form').reset();
             return [name.value, price.value, description.value];
 
         } else {
 
             alert("Erreur de format");
+            document.querySelector("#messageAlert").innerHTML =
+                "<div class=\"alert alert-danger\" role=\"alert\">Erreur de format</div>";
             return false;
 
         }
@@ -51,7 +61,10 @@ function FormValidation() {
     }
 
 }
-
-function DropPrice() {
-    document.querySelector()
-}
+document.onclick = function DropPrice(event) {
+    if (event.target.getAttribute("data-delete") !== null) {
+        event.preventDefault();
+        let target= 'target' in event? event.target : event.srcElement;
+        target.parentNode.parentElement.remove();
+    }
+};
